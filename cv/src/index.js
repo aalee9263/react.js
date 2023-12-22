2a05e9589bac21041d7257056cb79eacbcf45064
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-23 00:16:31 +0500

    css

--- cv/src/index.js
+++ cv/src/index.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import reactDOM from 'react-dom'
+import reactDOM from 'react-dom';
+import  "./index.css"
+
 
 function Hi () {
 
-  return<div>Hello World!</div>;
+  return<div>
+
+    <div className='Header'>Arshad Ali
+    
+    <nav> 
+      <ul>
+        <li>  Portfolio</li>
+        <li>  Contact</li>
+        <li>  Experience</li>
+        <li>  ABC Items 4</li>
+      </ul>
+    </nav>
+
+</div>
+    
+
+  </div>;
+
 }
 
 reactDOM.render(<Hi/>, document.querySelector('#root'));
