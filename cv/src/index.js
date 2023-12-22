1fc3eb64555c195aa5ac58d07882b7025fab63ff
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-23 01:25:58 +0500

    cv prapared

--- cv/src/index.js
+++ cv/src/index.js
@@ -1,28 +1,94 @@
-import React from 'react'
-import reactDOM from 'react-dom';
-import  "./index.css"
+import React from 'react';
+import reactDOM from 'react-dom'
+import './index.css';
+// import arshad from './arshad.jpg'
 
+function CV() {
+  return (
+    <html lang="en">
+      <head>
+        <title>CV Using HTML</title>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+        <div className="container">
+          <div className="header">
+            <div className="img-area">
+              {/* <img src={arshad.jpg} alt="Image"/> */}
+            </div>
+            <h1>Arshad Ali</h1>
+            <h3>AI Chatbot Developer</h3>
+          </div>
 
-function Hi () {
+          <div className="main">
+            <div className="left">
+              <h2>Personal Information</h2>
+              <p>
+                <strong>Name:</strong> Arshad Ali
+              </p>
+              <p>
+                <strong>Age:</strong> 32 Years
+              </p>
+              <p>
+                <strong>Email:</strong>aalee9263@gmail.com
+              </p>
+              <p>
+                <strong>Whatsapp:</strong>+92-327-7824990
+              </p>
+              <h2>Technical Skills</h2>
+              <ul>
+                <li>HTML/CSS</li>
+                <li>JAVAScrpit</li>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>Shorthand</li>
+              </ul>
+              <h2>Soft Skills</h2>
+              <ul>
+                <li>Teaching</li>
+                <li>Team Work</li>
+                <li>Problem Solving</li>
+                <li>Communication</li>
+                <li>Time management</li>
+              </ul>
+            </div>
 
-  return<div>
-
-    <div className='Header'>Arshad Ali
-    
-    <nav> 
-      <ul>
-        <li>  Portfolio</li>
-        <li>  Contact</li>
-        <li>  Experience</li>
-        <li>  ABC Items 4</li>
-      </ul>
-    </nav>
-
-</div>
-    
-
-  </div>;
-
+            <div className="right">
+              <h2>Education</h2>
+              <h3>B.Sc in Mathematics</h3>
+              <p>Islamia University of Bahawalpur - 2017</p>
+              <h3>DAE in Mechanical Techonology</h3>
+              <p>Swedish Institute of Technology - 2013</p>
+              <h2>Work Experience</h2>
+              <h3>Rockwell Corporation</h3>
+              <p>
+                <strong>Position:</strong> PA to Chairmen
+              </p>
+              <p>
+                <strong>Duration:</strong> 2017-2018
+              </p>
+              <h3>Nadeem & Company</h3>
+              <p>
+                <strong>Position:</strong> Assistant Manager
+              </p>
+              <p>
+                <strong>Duration:</strong> 2014-2016
+              </p>
+              <h3>Teaching</h3>
+              <p>
+                <strong>Position:</strong> Senior Science Teacher
+              </p>
+              <p>
+                <strong>Duration:</strong> 2012-2013
+              </p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
 }
 
-reactDOM.render(<Hi/>, document.querySelector('#root'));
+
+reactDOM.render(<CV/>, document.querySelector('#root'))
+export default CV;
