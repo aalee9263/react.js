6ebd5da92aeccef651004c8fb07436f743c4627e
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-25 01:07:10 +0500

    async

--- 5.async/src/index.js
+++ 5.async/src/index.js
@@ -1,10 +1,66 @@
-import React from 'react';
-import ReactDOM from "react-dom"
-import './index.css'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom'
+import "./index.css"
 
 
 const Home = () => {
 
-  const  [isLit, setLit] = react.useState(true);
-  const [temp, setTemp] = react
+  const  [isLit, setLit] = useState(true);
+  const [temp, setTemp] = useState(72);
+
+
+  // start here function of temp inc and dec with button
+  const sub = () => {
+
+    // setTemp(temp -1); //72 
+
+    setTemp((prev)=>{
+      // my logic here
+      return prev -1;
+    })
+
+    // setTemp((prev)=>{
+    //   // my logic here
+    //   return prev -1;
+    // })
+    // console.log("sub:", temp)
+  }
+
+  const add = () => {
+
+    setTemp((prev)=>{
+      // my logic here
+      return prev +1;
+    })
+  } 
+  console.log("add:", temp)
+
+  // // start here function of Light
+  // const changeState = () => {
+  //   setLit(!isLit)
+  // }
+
+return <div className={`room ${(isLit) ? "lit" : "dark"}`}>
+  <p>
+    <button onClick={sub}>-</button>
+    {temp}
+    <button onClick={add}>+</button>
+  </p>
+
+  <br/>
+<br/>
+
+<p>Light is {(isLit) ? "On" : "Off"}</p>
+<button onClick={()=> {setLit(!isLit)}}>
+  Turn {(isLit) ? "On" : "Off"}</button>
+
+
+
+
+</div>
+
 }
+
+
+
+ReactDOM.render(<Home/>, document.querySelector("#root"));
