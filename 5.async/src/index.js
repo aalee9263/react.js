e22090d7dc238cecc40b79516487fead690f29de
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-25 00:11:42 +0500

    initial

---
+++ 5.async/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom"
+import './index.css'
+
+
+const Home = () => {
+
+  const  [isLit, setLit] = react.useState(true);
+  const [temp, setTemp] = react
+}
