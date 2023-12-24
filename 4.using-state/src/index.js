45e75d31a9ec4c15c673685dd7c91ab47bc2b1b6
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-24 23:13:29 +0500

    completed useState

--- 4.using-state/src/index.js
+++ 4.using-state/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Home = () => {
+  const [isLit, setLit] = useState(true);
+
+  // const changeState = () => {
+  //   setLit(!isLit) one another way as mentioned in line no 16.
+  //   <button onClick={()=> setLit(!isLit)}>Change State</button>
+  // }
+
+  // return <div className={(isLit) ? "room lit" : "room dark"}>  
+  return <div className={`room ${(isLit) ? "lit" : "dark"}`}>  
+    <p>Light is {(isLit) ? "On" : "Off"}</p>
+    {/* <button onClick={()=> setLit(!isLit)}>Change State</button> one anoother way is under mentioned */}
+    <button onClick={()=> setLit(!isLit)}>
+      
+      Turn {(isLit) ? "Off" : "On"}
+      
+      </button>
+    </div>
+}
+
+ReactDOM.render(<Home/>, document.querySelector('#root'))
