c7b9148847172e5be6d17cabf54b7c65af62c310
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-25 16:16:47 +0500

    getting value

--- 6.weather-app/src/App.jsx
+++ 6.weather-app/src/App.jsx
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App () {
+
+const getWeather = (event) => {
+  event.preventDefault();
+  const cityName = document.querySelector("#cityName").value;
+  console.log(`getting weather of ${cityName}....`)
+};
+
+
   return (
     <div>
     <h1>Weather App</h1>
 
-    <form>
+
+    <form onSubmit={getWeather}>
       <label htmlFor="cityName">City:</label>
-      <input type="text" id="cityName" maxLength={20} minLength={2} />
+      <input type="text" id="cityName" maxLength={20} minLength={2} required />
     </form>
     <br />
     <button type='submit'> Get Weather </button>
