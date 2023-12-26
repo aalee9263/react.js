3546641cac6d1ca74c9b8de699f0311a06e3864f
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-26 19:13:08 +0500

    api putup

--- 6.weather-app/src/App.jsx
+++ 6.weather-app/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import axios from 'axios'
 
 import logo from './logo.svg';
 import './App.css';
@@ -6,13 +7,23 @@ import './App.css';
 function App () {
 
 const [cityName, setCityName] = useState("");
+const inputRef = useRef(null);
 
-const getWeather = (event) => {
+
+const getWeather = async(event) => {
   event.preventDefault();
   // under mentio method of taking input is not prefered..
   // const cityName = document.querySelector("#cityName").value;
   // console.log(`getting weather of ${cityName}....`)  // another way is under mention....
-  console.log(`getting weather of ${cityName}....`)  
+  // console.log(`getting weather of ${cityName}....`)// anothery way is under mention of taking inut value  
+  console.log(`getting weather of ${inputRef.current.value}....`)
+  
+try {  const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=62ba1f4835a74125a99140600232612&q=${inputRef.current.value}&aqi=no`)
+
+     console.log("response:", response.data)
+    } catch (e) {
+
+    }
 };
 
 const changeHandler = (event) => {
@@ -26,8 +37,15 @@ const changeHandler = (event) => {
     <h1>Weather App</h1>
     <form onSubmit={getWeather}>
       <label htmlFor="cityName">City:</label>
-      <input type="text" id="cityName" maxLength={20} minLength={2} required 
-      onChange={changeHandler}/>
+      <input 
+        type="text" 
+        id="cityName" 
+        maxLength={20} 
+        minLength={2} 
+        required 
+        onChange={changeHandler} 
+        ref={inputRef}
+        />
     <br />
     <button type="submit"> Get Weather </button>
     </form>
