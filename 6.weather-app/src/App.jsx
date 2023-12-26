b6a108e248b61226e7d71ef30eb83f99317affc4
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-26 17:39:16 +0500

    way to input

--- 6.weather-app/src/App.jsx
+++ 6.weather-app/src/App.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App () {
 
+const [cityName, setCityName] = state("")
+
 const getWeather = (event) => {
   event.preventDefault();
-  const cityName = document.querySelector("#cityName").value;
-  console.log(`getting weather of ${cityName}....`)
+  // under mentio method of taking input is not prefered..
+  // const cityName = document.querySelector("#cityName").value;
+  // console.log(`getting weather of ${cityName}....`)  // another way is under mention....
 };
+
+const changeHandler = (event) => {
+  setCityName(event.targe.value)
+}
 
 
   return (
@@ -17,7 +26,8 @@ const getWeather = (event) => {
 
     <form onSubmit={getWeather}>
       <label htmlFor="cityName">City:</label>
-      <input type="text" id="cityName" maxLength={20} minLength={2} required />
+      <input type="text" id="cityName" maxLength={20} minLength={2} required 
+      onChange={changeHandler}/>
     </form>
     <br />
     <button type='submit'> Get Weather </button>
