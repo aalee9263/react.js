a23c03769ae2d03e4f5f61502aee431024077dab
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-26 17:49:36 +0500

    console problem

--- 6.weather-app/src/App.jsx
+++ 6.weather-app/src/App.jsx
@@ -5,32 +5,32 @@ import './App.css';
 
 function App () {
 
-const [cityName, setCityName] = state("")
+const [cityName, setCityName] = useState("");
 
 const getWeather = (event) => {
   event.preventDefault();
   // under mentio method of taking input is not prefered..
   // const cityName = document.querySelector("#cityName").value;
   // console.log(`getting weather of ${cityName}....`)  // another way is under mention....
+   console.log(`getting weather of ${cityName}....`)  
 };
 
 const changeHandler = (event) => {
-  setCityName(event.targe.value)
+  setCityName(event.target.value)
+  console.log("changeHandler:", event.target.value)
 }
 
 
   return (
     <div>
     <h1>Weather App</h1>
-
-
     <form onSubmit={getWeather}>
       <label htmlFor="cityName">City:</label>
       <input type="text" id="cityName" maxLength={20} minLength={2} required 
       onChange={changeHandler}/>
     </form>
     <br />
-    <button type='submit'> Get Weather </button>
+    <button type="submit"> Get Weather </button>
     </div>
   );
 }
