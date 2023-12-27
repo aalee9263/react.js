0a810972574f2e59347811db0f17d74e47b624ff
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-27 16:10:24 +0500

    CSS ADDED

--- 6.weather-app/src/components/WeatherCard.jsx
+++ 6.weather-app/src/components/WeatherCard.jsx
@@ -3,15 +3,12 @@ import './WeatherCard.css'
 const WeatherCard = ({data}) => {
     return (
         <div className='WeatherCard'>
-        {data?.location?.name} <br /> {data?.location?.country}
-        <br />
+        {data?.location?.name}, {data?.location?.country}
         <div className='temp'> {data?.current?.temp_c} °C </div>
-        <br />
+        <img src={data?.current?.condition?.icon} alt="icon" />
         <div className='weather'> {data?.current?.condition?.text} </div>
-        <br />
         <div className='wind'>Wind: {data?.current?.wind_kph} 
         KPH </div>
-        <br />
       </div>
   
     )
